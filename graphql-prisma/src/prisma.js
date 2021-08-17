6ce56a350f78668da8465344f334ddce190ce981
Chris Philbin <chris@chrisphilbin.net>
2021-08-17 15:46:08 -0400

    Updating data models

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,7 +5,21 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4455",
 });
 
+prisma.exists
+  .User({
+    id: "cksejw4l2002b0834yy3zqovd",
+  })
+  .then((exists) => console.log(exists));
+
 const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({
+    id: authorId,
+  });
+
+  if (!userExists) {
+    throw new Error("User not found.");
+  }
+
   const post = await prisma.mutation.createPost(
     {
       data: {
@@ -17,24 +31,40 @@ const createPostForUser = async (authorId, data) => {
         },
       },
     },
-    "{ id }"
+    "{ author { id name email posts { id title published } } }"
   );
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: authorId,
-      },
-    },
-    "{ id name email posts { id title published } }"
-  );
-  return user;
+  return post.author;
 };
 
-createPostForUser("cksejw4l2002b0834yy3zqovd", {
-  title: "Great books",
-  body: "War of art",
-  published: true,
-}).then((user) => console.log(JSON.stringify(user)));
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({
+    id: postId,
+  });
+
+  if (!postExists) {
+    throw new Error("Post not found.");
+  }
+
+  const post = await prisma.mutation.updatePost(
+    {
+      where: {
+        id: postId,
+      },
+      data,
+    },
+    "{  author { id name email posts { id title published } } }"
+  );
+
+  return post.author;
+};
+
+// createPostForUser("cksejw4l2002b0834yy3zqovd", {
+//   title: "Great books",
+//   body: "War of art",
+//   published: true,
+// })
+//   .then((user) => console.log(JSON.stringify(user)))
+//   .catch((error) => console.log(error.message));
 
 // prisma.mutation
 //   .createPost(
