f71feece60f12e074ae7903c6f904062cd46c8dd
Chris Philbin <chris@chrisphilbin.net>
2021-08-24 07:07:30 -0400

    Addressing edge cases

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -88,6 +88,21 @@ const Mutation = {
       },
     });
 
+    const postIsPublished = await prisma.exists.Post({
+      id: args.id,
+      published: true,
+    });
+
+    if (postIsPublished && args.data.published === false) {
+      await prisma.mutation.deleteManyComments({
+        where: {
+          post: {
+            id: args.id,
+          },
+        },
+      });
+    }
+
     if (!postExists) {
       throw new Error("Cannot edit post.");
     }
@@ -125,8 +140,17 @@ const Mutation = {
       info
     );
   },
-  createComment(parent, args, { prisma, request }, info) {
+  async createComment(parent, args, { prisma, request }, info) {
     const userId = getUserId(request);
+
+    const postExists = await prisma.exists.Post({
+      id: args.data.post,
+      published: true,
+    });
+
+    if (!postExists) {
+      throw new Error("Unable to find post.");
+    }
 
     return prisma.mutation.createComment(
       {
