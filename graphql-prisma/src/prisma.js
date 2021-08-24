4e7136bad6a161613c73f25047fe7f66c0a8ae14
Chris Philbin <chris@chrisphilbin.net>
2021-08-23 23:26:08 -0400

    Adding fragments

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,9 +1,11 @@
 import { Prisma } from "prisma-binding";
+import { fragmentReplacements } from "./resolvers/index";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: "http://localhost:4455",
   secret: "thisisthesecrettext",
+  fragmentReplacements,
 });
 
 export default prisma;
